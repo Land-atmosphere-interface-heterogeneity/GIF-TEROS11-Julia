06c263fa7d8ebaba43dc0663681bf9ac7e7d7327
AlexisRenchon <a.renchon@gmail.com>
2019-12-17 13:09:13 -0600

    Update DownloadZentradata.py

--- DownloadZentradata.py
+++ DownloadZentradata.py
@@ -4,23 +4,10 @@ from os import getenv
 token = ZentraToken(username=getenv("zentra_un"),
                     password=getenv("zentra_pw"))
 
-# Get the settings for a device
-settings = ZentraSettings(sn="06-00187",
-                          token=token)
-# Report the measurement settings
-settings.measurement_settings
-
-# Get the status messages for a device
-status = ZentraStatus(sn="06-00187",
-                      token=token)
-# Report the cellular statuses
-status.cellular_statuses
+token.token
 
 # Get the readings for a device
-readings = ZentraReadings(sn="06-00761",
-                          token=token,
-                          start_time=int(datetime.datetime(year=2019,
-                                                           month=7,
-                                                           day=4).timestamp()))
+readings = ZentraReadings(sn="z6-04628",token=token,start_time=int(datetime.datetime(year=2019,month=11,day=27).timestamp()))
+
 # Report the readings from the first ZentraTimeseriesRecord
 readings.timeseries[0].values
