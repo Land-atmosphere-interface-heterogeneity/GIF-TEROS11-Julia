154ce38aecc97c93ec70bec62f1ce62efabb6643
AlexisRenchon <a.renchon@gmail.com>
2019-12-18 14:46:07 -0600

    Update DownloadZentradata.py

--- DownloadZentradata.py
+++ DownloadZentradata.py
@@ -6,7 +6,6 @@ Device_SN = ["z6-04628","z6-04625","z6-04619","z6-04629","z6-04641","z6-04638","
 
 for i in range(0,11):
     readings = ZentraReadings(sn=Device_SN[i],token=token,start_time=int(datetime.datetime(year=2019,month=11,day=27).timestamp()))
-    data = readings.timeseries[0].values
-    data.to_csv(path_or_buf=str(i+1)+"_"+sn_ID+".csv")
+    readings.timeseries[0].values.to_csv(path_or_buf=str(i+1)+"_"+sn_ID+".csv")
     time.sleep(30) # The loop will crash if we don't wait a bit. METER configuration. 
 
