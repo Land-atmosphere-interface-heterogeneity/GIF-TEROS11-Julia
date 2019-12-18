608dd96dfd9c24552b311465a725d8c52362801e
AlexisRenchon <a.renchon@gmail.com>
2019-12-18 10:48:35 -0600

    Update DownloadZentradata.py

--- DownloadZentradata.py
+++ DownloadZentradata.py
@@ -3,10 +3,10 @@ from os import getenv
 
 token = ZentraToken(username=getenv("zentra_un"),password=getenv("zentra_pw"))
 
-token.token
+Device_SN = ["z6-04628","z6-04625","z6-04619","z6-04629","z6-04641","z6-04638","z6-04624","z6-04627","z6-04620","z6-04621","z6-04618"]
 
-# Get the readings for a device
-readings = ZentraReadings(sn="z6-04628",token=token,start_time=int(datetime.datetime(year=2019,month=11,day=27).timestamp()))
-
-# Report the readings from the first ZentraTimeseriesRecord
-readings.timeseries[0].values
+for i in Device_SN
+    readings = ZentraReadings(sn=i,token=token,start_time=int(datetime.datetime(year=2019,month=11,day=27).timestamp()))
+    data = readings.timeseries[0].values
+    import pandas as pd
+    data.to_csv(path_or_buf=i+".csv")
