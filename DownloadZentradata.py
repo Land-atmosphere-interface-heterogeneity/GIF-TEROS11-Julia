5df006ab416aea96403904f8a93bc6515d44e7f6
AlexisRenchon <a.renchon@gmail.com>
2019-12-18 15:10:04 -0600

    Update DownloadZentradata.py

--- DownloadZentradata.py
+++ DownloadZentradata.py
@@ -3,5 +3,5 @@ token = ZentraToken(username=os.getenv("zentra_un"),password=os.getenv("zentra_p
 Device_SN = ["z6-04628","z6-04625","z6-04619","z6-04629","z6-04641","z6-04638","z6-04624","z6-04627","z6-04620","z6-04621","z6-04618"]
 for i in range(0,11):
     readings = ZentraReadings(sn=Device_SN[i],token=token,start_time=int(datetime.datetime(year=2019,month=11,day=27).timestamp()))
-    readings.timeseries[0].values.to_csv(path_or_buf=str(i+1)+"_"+sn_ID+".csv")
+    readings.timeseries[0].values.to_csv(path_or_buf=str(i+1)+"_"+Device_SN[i]+".csv")
     time.sleep(120) # The loop will crash if we don't wait a bit. METER configuration.
