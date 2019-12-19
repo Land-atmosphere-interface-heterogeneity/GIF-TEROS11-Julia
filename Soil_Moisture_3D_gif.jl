cb8d6cca57a2db35c119f302b42c015a308a9445
AlexisRenchon <a.renchon@gmail.com>
2019-12-19 16:40:58 -0600

    Added mean SWC along with Precip timeseries subplot

--- Soil_Moisture_3D_gif.jl
+++ Soil_Moisture_3D_gif.jl
@@ -4,7 +4,7 @@
 cd("C:\\Users\\arenchon\\Documents\\GitHub\\GIF-TEROS11-Julia")
 
 # Used Packages
-using DataFrames; using CSV; using Dates; using Plots;
+using DataFrames; using CSV; using Dates; using Plots; using Statistics;
 
 # First part of this code is similar to Plot_input.jl
 Input_FN = readdir("Input\\TEROS\\")
@@ -40,11 +40,7 @@ end
 MD = CSV.read("Input\\Metadata.csv")
 x = MD.x*12.5
 y = MD.y*12.5
-# Replace missing by NaN, for plotting
-x = replace(x, missing=>NaN)
-y = replace(y, missing=>NaN)
-SWC = replace(SWC, missing=>NaN)
-SWC = replace(SWC, 0.0=>NaN)
+SWC = replace(SWC, 0.0=>missing)
 # could find something more elegant to do the loop below...
 for j = 1:66
     for i = 1:m
@@ -96,14 +92,14 @@ end
 # TO DO: Make the script run automatically daily (or weekly).
 
 # Need same datetime (daily) for SWC data and met data
-n_all = floor(Int64, length(Dtime)/48)
-Dtime_all = Array{DateTime}(undef,n_all)
+Dtime_all = collect(Date(2019, 11, 23):Day(1):today())
+n_all = length(Dtime_all)
 SWC_daily_mean = Array{Float64}(undef,n_all)
 SWC_daily = Array{Union{Float64,Missing}}(missing,n_all,66)
 Precip_daily = Array{Float64}(undef,n_all)
-Dtime_all = collect(Date(2019, 11, 23):Day(1):today())
 for i = collect(1:n_all)
     SWC_daily[i,:] = SWC[25+(i-1)*48,:]
+    SWC_daily_mean[i] = mean(skipmissing(SWC[i,:]))
     t = findfirst(x -> x == Dtime_all[i],Dtime_met_d)
     if isnothing(t) == false
         Precip_daily[i] = Precip_d[t]
@@ -112,7 +108,7 @@ end
 
 anim = @animate for i = collect(1:1:n_all)
     z = SWC_daily[i,:]
-    use = findall(!isnan,z) # all non NaN values in z
+    use = findall(!ismissing,z) # all non missing values in z
     p1 = scatter(x[use],y[use],color=:redsblues,markersize=10,zcolor=z[use],
     xlabel="x (m)",ylabel="y (m)",title=Dates.format(Dtime_all[i], "e, dd u yyyy"),
     xticks = 0:12.5:87.5,yticks = 0:12.5:87.5,colorbar_title = "Soil Moisture",
@@ -120,8 +116,11 @@ anim = @animate for i = collect(1:1:n_all)
     p2 = bar(Dtime_all[1:i],Precip_daily[1:i],
     xlims=(Dates.value(Dtime_all[1]),Dates.value(Dtime_all[n_all])),
     ylims=(0,maximum(Precip_daily)),ylabel="Precip (mm)");
+    plot!(twinx(),Dtime_all[1:i],SWC_daily_mean[1:i],
+    xlims=(Dates.value(Dtime_all[1]),Dates.value(Dtime_all[n_all])),
+    ylims=(0.414,0.417),ylabel="SWC")
     l = @layout [a{0.8h}; b]
-    plot(p1, p2, layout=l,size=(500,700))
+    plot(p1, p2, layout=l,size=(500,600))
     #plot!(legend = nothing)
 end
 gif(anim,"Output\\anim_5days_v3.gif",fps=5)
