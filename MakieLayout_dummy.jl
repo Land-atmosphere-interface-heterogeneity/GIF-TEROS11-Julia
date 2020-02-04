cf0bc4e790fc077ba39245e3fc8704bcd76d66de
AlexisRenchon <a.renchon@gmail.com>
2020-02-03 22:49:58 -0600

    Update MakieLayout_dummy.jl

--- MakieLayout_dummy.jl
+++ MakieLayout_dummy.jl
@@ -20,16 +20,16 @@ dateticks = optimize_ticks(Dtime_all[1],Dtime_all[end])[1]
 # Create Scene and 2D axis
 scene, layout = layoutscene(10, 24, 10, resolution = (1500, 900))
 ax = Array{LAxis}(undef,4)
-sl = layout[10, 1:24] = LSlider(scene, range=1:n_all)
+sl = layout[10, 1:23] = LSlider(scene, range=1:n_all)
 
-Text_date = layout[1,1:24] = LText(scene, text= lift(X->Dates.format(Dtime_all[X], "e, dd u yyyy"), sl.value), textsize=40)
+Text_date = layout[1,1:23] = LText(scene, text= lift(X->Dates.format(Dtime_all[X], "e, dd u yyyy"), sl.value), textsize=40)
 # SWC + Precip time-series (bottom)
-ax[3] = layout[8:9, 1:24] = LAxis(scene, ylabel = "Precip", yaxisposition = :right, xticklabelsvisible = false, xticksvisible = false, ylabelpadding = -25, xgridvisible = false, ygridvisible = false)
+ax[3] = layout[8:9, 1:23] = LAxis(scene, ylabel = "Precip", yaxisposition = :right, xticklabelsvisible = false, xticksvisible = false, ylabelpadding = -10, xgridvisible = false, ygridvisible = false, yticklabelalign = (:left, :center))
 barplot!(ax[3], Dtime_all_rata[1:end], Precip_daily[1:end], color = :blue)
 scatter!(ax[3], lift(X-> [Point2f0(Dtime_all_rata[X], 0)], sl.value), marker = :vline, markersize = Vec2f0(0.5,5), color = :black)
 xlims!(ax[3], (Dtime_all_rata[1], Dtime_all_rata[end])); ylims!(ax[3], (0, 2))
 
-ax[1] = layout[8:9, 1:24] = LAxis(scene, ylabel="SWC", xlabel="Date", ylabelpadding = -25)
+ax[1] = layout[8:9, 1:23] = LAxis(scene, ylabel="SWC", xlabel="Date", ylabelpadding = -25)
 lines!(ax[1], Dtime_all_rata[1:end], SWC_daily_mean[1:end], color = :blue, linewidth = 2)
 band!(ax[1], Dtime_all_rata[1:end], SWC_daily_mean[1:end] + SWC_daily_std[1:end], SWC_daily_mean[1:end] - SWC_daily_std[1:end], color = color = RGBAf0(0,0,1,0.3))
 xlims!(ax[1], (Dtime_all_rata[1], Dtime_all_rata[end]));
@@ -37,14 +37,14 @@ ax[1].xticks[] = ManualTicks(datetime2rata.(dateticks) , Dates.format.(dateticks
 
 
 # Tsoil + Rsoil time-series (bottom)
-ax[2] = layout[6:7, 1:24] = LAxis(scene, ylabel="Ts", ylabelpadding = -25, xticklabelsvisible = false, xticksvisible = false)
+ax[2] = layout[6:7, 1:23] = LAxis(scene, ylabel="Ts", ylabelpadding = -25, xticklabelsvisible = false, xticksvisible = false)
 lines!(ax[2], Dtime_all_rata[1:end], Tsoil_daily_mean[1:end], color = :red, linewidth = 2)
 band!(ax[2], Dtime_all_rata[1:end], Tsoil_daily_mean[1:end] + Tsoil_daily_std[1:end], Tsoil_daily_mean[1:end] - Tsoil_daily_std[1:end], color = RGBAf0(1,0,0,0.3))
 scatter!(ax[2], lift(X-> [Point2f0(Dtime_all_rata[X], 0)], sl.value), marker = :vline, markersize = Vec2f0(0.5,5), color = :black)
 xlims!(ax[2], (Dtime_all_rata[1], Dtime_all_rata[end]));
 ax[2].xticks[] = ManualTicks(datetime2rata.(dateticks) , Dates.format.(dateticks, "yyyy-mm-dd"))
 
-ax[4] = layout[6:7, 1:24] = LAxis(scene, ylabel = "Rs", xticklabelsvisible = false, xticksvisible = false, xgridvisible = false, ygridvisible = false, yaxisposition = :right, ylabelpadding = -25)
+ax[4] = layout[6:7, 1:23] = LAxis(scene, ylabel = "Rs", xticklabelsvisible = false, xticksvisible = false, xgridvisible = false, ygridvisible = false, yaxisposition = :right, ylabelpadding = -10, yticklabelalign = (:left, :center))
 lines!(ax[4], Dtime_all_rata[1:end], Rsoil_daily_mean[1:end], color = :black)
 xlims!(ax[4], (Dtime_all_rata[1], Dtime_all_rata[end]));
 
@@ -53,21 +53,21 @@ cbar = Array{LColorbar}(undef,3)
 
 ax3D[1] = layout[1:7, 1:8] = LRect(scene, visible = false);
 scene3D_1 = Scene(scene, lift(IRect2D, ax3D[1].layoutnodes.computedbbox), camera = cam3d!, raw = false, show_axis = true);
-surface!(scene3D_1, 0:7, 0:7, elev, color = lift(X-> GLMakie.vec2color(Matrix(sparse(x, y, SWC_daily[X,:])), Reverse(:lighttest), (0,1)), sl.value), shading = false, limits = Rect(0, 0, 0, 7, 7, 2));
+surface!(scene3D_1, 0:7, 0:7, elev, color = lift(X-> GLMakie.vec2color(Matrix(sparse(x, y, SWC_daily[X,:])), Reverse(:lighttest), (0,1)), sl.value), shading = false, limits = Rect(0, 0, 0, 7, 7, 1.5));
 x_or = [0,0,0]; Ylen = 7; Zlen = 1; Xlen = 7;
 mesh!(scene3D_1, lift(X-> HyperRectangle(Vec3f0(x_or), Vec3f0(Xlen, Ylen, Wtable_daily[X])), sl.value) , color = RGBAf0(0,0,1,0.2));
 cbar[1] = layout[2, 2:7] = LColorbar(scene, height = 20, limits = (0, 1), label = "SWC", colormap = :lighttest, vertical = false, labelpadding = -5);
 
 ax3D[2] = layout[1:7, 9:16] = LRect(scene, visible = false);
 scene3D_2 = Scene(scene, lift(IRect2D, ax3D[2].layoutnodes.computedbbox), camera = cam3d!, raw = false, show_axis = true);
-surface!(scene3D_2, 0:7, 0:7, elev, color = lift(X-> GLMakie.vec2color(Matrix(sparse(x, y, Tsoil_daily[X,:])), Reverse(:lighttest), (0,1)), sl.value), shading = false, limits = Rect(0, 0, 0, 7, 7, 2));
+surface!(scene3D_2, 0:7, 0:7, elev, color = lift(X-> GLMakie.vec2color(Matrix(sparse(x, y, Tsoil_daily[X,:])), Reverse(:lighttest), (0,1)), sl.value), shading = false, limits = Rect(0, 0, 0, 7, 7, 1.5));
 x_or = [0,0,0]; Ylen = 7; Zlen = 1; Xlen = 7;
 mesh!(scene3D_2, lift(X-> HyperRectangle(Vec3f0(x_or), Vec3f0(Xlen, Ylen, Wtable_daily[X])), sl.value) , color = RGBAf0(0,0,1,0.2));	
 cbar[2] = layout[2, 10:15] = LColorbar(scene, height = 20, limits = (0, 1), label = "Tsoil", colormap = :lighttest, vertical = false, labelpadding = -5);
 
 ax3D[3] = layout[1:7, 17:24] = LRect(scene, visible = false);
 scene3D_3 = Scene(scene, lift(IRect2D, ax3D[3].layoutnodes.computedbbox), camera = cam3d!, raw = false, show_axis = true);
-surface!(scene3D_3, 0:7, 0:7, elev, color = lift(X-> GLMakie.vec2color(Matrix(sparse(x, y, Rsoil_daily[X,:])), Reverse(:lighttest), (0,1)), sl.value), shading = false, limits = Rect(0, 0, 0, 7, 7, 2));
+surface!(scene3D_3, 0:7, 0:7, elev, color = lift(X-> GLMakie.vec2color(Matrix(sparse(x, y, Rsoil_daily[X,:])), Reverse(:lighttest), (0,1)), sl.value), shading = false, limits = Rect(0, 0, 0, 7, 7, 1.5));
 x_or = [0,0,0]; Ylen = 7; Zlen = 1; Xlen = 7;
 mesh!(scene3D_3, lift(X-> HyperRectangle(Vec3f0(x_or), Vec3f0(Xlen, Ylen, Wtable_daily[X])), sl.value) , color = RGBAf0(0,0,1,0.2));	  
 cbar[3] = layout[2, 18:23] = LColorbar(scene, height = 20, limits = (0, 1), label = "Rsoil", colormap = :lighttest, vertical = false, labelpadding = -5);
