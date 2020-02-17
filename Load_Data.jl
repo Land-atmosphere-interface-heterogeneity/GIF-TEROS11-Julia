bca1ad613cac1318d8c44753d0cf903e55b323bd
AlexisRenchon <a.renchon@gmail.com>
2020-02-17 13:13:01 -0600

    Added MalieLayout_data

--- Load_Data.jl
+++ Load_Data.jl
@@ -1,31 +1,48 @@
 # Load Packages
 using CSV, DataFrames, Dates, Statistics
-
 # Set working directory
 cd("C:\\Users\\arenchon\\Documents\\GitHub\\GIF-TEROS11-Julia")
 
 # Load data from TEROS input files
-Input_FN = readdir("Input\\TEROS\\")
-permute!(Input_FN,[3,4,5,6,7,8,9,10,11,1,2]) # need to reorder from 1 to 11
-n = length(Input_FN) # this is the number of input files, useful later
-data = DataFrame[]
-[push!(data, CSV.read("Input\\TEROS\\"*Input_FN[i], dateformat="yyyy-mm-dd HH:MM:SS+00:00")) for i in 1:n]
+function loadteros()
+	Input_FN = readdir("Input\\TEROS\\")
+	permute!(Input_FN,[3,4,5,6,7,8,9,10,11,1,2]) # need to reorder from 1 to 11
+	n = length(Input_FN) # this is the number of input files, useful later
+	data = DataFrame[]
+	[push!(data, CSV.read("Input\\TEROS\\"*Input_FN[i], dateformat="yyyy-mm-dd HH:MM:SS+00:00")) for i in 1:n]
+	return data
+end
+data = loadteros();
 
 # Download met data
 [download("http://www.atmos.anl.gov/ANLMET/numeric/2019/"*i*"19met.data", "Input\\MET TOWER\\"*i*"19met.data") for i in ["nov", "dec"]]
-
 # Load that met data
-col_name = [:DOM,:Month,:Year,:Time,:PSC,:WD60,:WS60,:WD_STD60,:T60,:WD10,:WS10,:WD_STD10,:T10,:DPT,:RH,:TD100,:Precip,:RS,:RN,:Pressure,:WatVapPress,:TS10,:TS100,:TS10F]
-metdata = DataFrame[]
-[push!(metdata, CSV.read("Input\\MET TOWER\\"*i*"19met.data", delim=' ', header=col_name, ignorerepeated=true, datarow=1, footerskip=2)) for i in ["nov", "dec"]]
-metdata = reduce(vcat, metdata)
-met_n = size(metdata,1)
+function loadmet()
+	col_name = [:DOM,:Month,:Year,:Time,:PSC,:WD60,:WS60,:WD_STD60,:T60,:WD10,:WS10,:WD_STD10,:T10,:DPT,:RH,:TD100,:Precip,:RS,:RN,:Pressure,:WatVapPress,:TS10,:TS100,:TS10F]
+	metdata = DataFrame[]
+	[push!(metdata, CSV.read("Input\\MET TOWER\\"*i*"19met.data", delim=' ', header=col_name, ignorerepeated=true, datarow=1, footerskip=2)) for i in ["nov", "dec"]]
+	metdata = reduce(vcat, metdata)
+end
+metdata = loadmet();
 
-# Rearrange those data in 1 dataframe
-Dtime = collect(Dates.DateTime(DateTime(2019, 11, 23, 00, 00, 00)):Dates.Minute(30):now())
-m = length(Dtime)
-nextit = 0:5:5*n-1
-function SWC_array(Dtime::Array{DateTime,1}, data::Array{DataFrame,1})
+# Load metadata (position of TEROS sensors)
+function loadmeta()
+	MD = CSV.read("Input\\Metadata.csv")
+	x = MD.x*12.5
+	y = MD.y*12.5
+	x = x[1:end .!= 35]; x = x[1:end .!= 35]
+	y = y[1:end .!= 35]; y = y[1:end .!= 35]
+	x = convert(Array{Float64,1}, x)
+	y = convert(Array{Float64,1}, y)
+	return x, y
+end
+x, y = loadmeta();
+
+function rearrangedata(data::Array{DataFrame,1}, metdata::DataFrame)
+	Dtime = collect(Dates.DateTime(DateTime(2019, 11, 23, 00, 00, 00)):Dates.Minute(30):now())
+        # Rearrange SWC and Tsoil
+	m = length(Dtime); n = 11 # 11 files, for 11 ZL6 datalogger
+	nextit = 0:5:5*n-1
 	SWC = Array{Union{Float64,Missing}}(missing, m, 66)
 	for j = 1:11 # For all files
 	    for i = 1:m
@@ -42,9 +59,7 @@ function SWC_array(Dtime::Array{DateTime,1}, data::Array{DataFrame,1})
 	    end
 	end
 	SWC = replace(SWC, 0.0=>missing)
-end
-SWC = SWC_array(Dtime, data)
-function Tsoil_array(Dtime::Array{DateTime,1}, data::Array{DataFrame,1})
+	SWC = SWC[:, 1:size(SWC, 2) .!= 35]; SWC = SWC[:, 1:size(SWC, 2) .!= 35] # Need to find how to delete 35 and 36 simultaneously
 	Tsoil = Array{Union{Float64,Missing}}(missing, m, 66)
 	for j = 1:11 # For all files
 	    for i = 1:m
@@ -61,63 +76,62 @@ function Tsoil_array(Dtime::Array{DateTime,1}, data::Array{DataFrame,1})
 	    end
 	end
 	Tsoil = replace(Tsoil, 0.0=>missing)
-end
-Tsoil = Tsoil_array(Dtime, data)
+        Tsoil = Tsoil[:, 1:size(Tsoil, 2) .!= 35]; Tsoil = Tsoil[:, 1:size(Tsoil, 2) .!= 35] # Need to find how to delete 35 and 36 simultaneously
 
-# Load metadata (position of TEROS sensors)
-MD = CSV.read("Input\\Metadata.csv")
-x = MD.x*12.5
-y = MD.y*12.5
+	# Create a DateTime vector from metdata Month, Year and Time
+	# First, need 4-digits Array for Time
+	met_n = size(metdata, 1) 
+	metdata_time_str = Array{String}(undef, met_n)
+	for i = 1:met_n
+	    if length(string(metdata.Time[i])) == 2 # if only 2 numbers
+		metdata_time_str[i] = "00$(metdata.Time[i])"
+	    elseif length(string(metdata.Time[i])) == 3 # if only 3 numbers
+		metdata_time_str[i] = "0$(metdata.Time[i])"
+	    elseif length(string(metdata.Time[i])) == 4 # 4 numbers
+		metdata_time_str[i] = string(metdata.Time[i])
+	    end
+	end
+	# Then, we can use day of month, month, year and time
+	Dtime_met = Array{DateTime}(undef, met_n)
+	for i = 1:met_n
+	    Dtime_met[i] = DateTime(metdata.Year[i]+2000,metdata.Month[i],metdata.DOM[i],parse(Int64,metdata_time_str[i][1:2]),parse(Int64,metdata_time_str[i][3:4]))
+	end
 
-# Create a DateTime vector from metdata Month, Year and Time
-# First, need 4-digits Array for Time
-metdata_time_str = Array{String}(undef,met_n)
-for i = 1:met_n
-    if length(string(metdata.Time[i])) == 2 # if only 2 numbers
-        metdata_time_str[i] = "00$(metdata.Time[i])"
-    elseif length(string(metdata.Time[i])) == 3 # if only 3 numbers
-        metdata_time_str[i] = "0$(metdata.Time[i])"
-    elseif length(string(metdata.Time[i])) == 4 # 4 numbers
-        metdata_time_str[i] = string(metdata.Time[i])
-    end
-end
-# Then, we can use day of month, month, year and time
-Dtime_met = Array{DateTime}(undef,met_n)
-for i = 1:met_n
-    Dtime_met[i] = DateTime(metdata.Year[i]+2000,metdata.Month[i],metdata.DOM[i],parse(Int64,metdata_time_str[i][1:2]),parse(Int64,metdata_time_str[i][3:4]))
-end
+	# Integrate daily Precip
+	# I need to redo this... this is not clean. Maybe fixing latest rain on ANLMET website should be done first. 
+	Precip_d = Array{Float64}(undef, 61)
+	Dtime_met_d = Array{DateTime}(undef, 61)
+	for i = 1:30
+	    use = findall(x -> Dates.day(x) == i && Dates.month(x) == 11, Dtime_met)
+	    Precip_d[i] = sum(metdata.Precip[use])
+	    Dtime_met_d[i] = Date(DateTime(2019,11,i))
+	end
+	for i = 1:31
+	    use = findall(x -> Dates.day(x) == i && Dates.month(x) == 12, Dtime_met)
+	    Precip_d[30 + i] = sum(metdata.Precip[use])
+	    Dtime_met_d[30 + i] = Date(DateTime(2019,12, i))
+	end
 
-# Integrate daily Precip
-# I need to redo this... this is not clean. Maybe fixing latest rain on ANLMET website should be done first. 
-Precip_d = Array{Float64}(undef,61)
-Dtime_met_d = Array{DateTime}(undef,61)
-for i = 1:30
-    use = findall(x -> Dates.day(x) == i && Dates.month(x) == 11,Dtime_met)
-    Precip_d[i] = sum(metdata.Precip[use])
-    Dtime_met_d[i] = Date(DateTime(2019,11,i))
-end
-for i = 1:31
-    use = findall(x -> Dates.day(x) == i && Dates.month(x) == 12,Dtime_met)
-    Precip_d[30 + i] = sum(metdata.Precip[use])
-    Dtime_met_d[30 + i] = Date(DateTime(2019,12,i))
-end
+	# Need same datetime (daily) for SWC data and met data
+	Dtime_all = collect(Date(2019, 11, 23):Day(1):today())
+	n_all = length(Dtime_all)
+	SWC_daily_mean = Tsoil_daily_mean = SWC_daily_std = Tsoil_daily_std = Array{Float64}(undef, n_all)
+	SWC_daily = Tsoil_daily = Array{Union{Float64,Missing}}(missing, n_all, 64)
+	Precip_daily = Array{Float64}(undef, n_all)
+	for i = collect(1:n_all-1) # up to day before today, in case it's before noon
+	    SWC_daily[i,:], Tsoil_daily[i, :] = SWC[25+(i-1)*48,:], Tsoil[25+(i-1)*48, :]
+	    SWC_daily_mean[i], Tsoil_daily_mean[i] = mean(skipmissing(SWC_daily[i, :])), mean(skipmissing(Tsoil_daily[i, :]))
+	    SWC_daily_std[i], Tsoil_daily_std[i] = std(skipmissing(SWC_daily[i, :])), std(skipmissing(Tsoil_daily[i, :]))
+	    t = findfirst(x -> x == Dtime_all[i], Dtime_met_d)
+	    if isnothing(t) == false
+		Precip_daily[i] = Precip_d[t]
+	    end
+	end
 
-# Need same datetime (daily) for SWC data and met data
-Dtime_all = collect(Date(2019, 11, 23):Day(1):today())
-n_all = length(Dtime_all)
-SWC_daily_mean = Tsoil_daily_mean = SWC_daily_std = Tsoil_daily_std = Array{Float64}(undef, n_all)
-SWC_daily = Tsoil_daily = Array{Union{Float64,Missing}}(missing, n_all,66)
-Precip_daily = Array{Float64}(undef, n_all)
-for i = collect(1:n_all-1) # up to day before today, in case it's before noon
-    SWC_daily[i,:], Tsoil_daily[i, :] = SWC[25+(i-1)*48,:], Tsoil[25+(i-1)*48, :]
-    SWC_daily_mean[i], Tsoil_daily_mean[i] = mean(skipmissing(SWC_daily[i, :])), mean(skipmissing(Tsoil_daily[i, :]))
-    SWC_daily_std[i], Tsoil_daily_std[i] = std(skipmissing(SWC_daily[i, :])), std(skipmissing(Tsoil_daily[i, :]))
-    t = findfirst(x -> x == Dtime_all[i], Dtime_met_d)
-    if isnothing(t) == false
-        Precip_daily[i] = Precip_d[t]
-    end
+	# Delete Precip outliers, daily rain > 50 mm which may be calibration day... this should be fixed by Evan in the qc data!!
+	Precip_daily[Precip_daily.>=50] .= 0
+	# Return what we need
+	return SWC_daily, SWC_daily_mean, SWC_daily_std, Tsoil_daily, Tsoil_daily_mean, Tsoil_daily_std, Precip_daily, Dtime_all
 end
-
-# Delete Precip outliers, daily rain > 50 mm which may be calibration day... this should be fixed by Evan in the qc data!!
-Precip_daily[Precip_daily.>=50] .= 0
+SWC_daily, SWC_daily_mean, SWC_daily_std, Tsoil_daily, Tsoil_daily_mean, Tsoil_daily_std, Precip_daily, Dtime_all = rearrangedata(data, metdata);
 
---
+++ MakieLayout_data.jl
@@ -0,0 +1,97 @@
+include("Load_Data.jl"); # Currently, @time ~20 sec first run, ~4 sec next runs. Could be improved!
+
+using Makie, MakieLayout, Dates, GLMakie, GeometryTypes, SparseArrays
+using PlotUtils: optimize_ticks
+
+n_all = size(SWC_daily, 1); # Below are random data, for now
+Rsoil_daily = rand(n_all, 64); Rsoil_daily_mean = rand(n_all, 1); Rsoil_daily_std = rand(n_all, 1);
+Wtable_daily = rand(n_all,1);
+elev = rand(8,8).+.4;
+Dtime_all_rata = datetime2rata.(Dtime_all);
+dateticks = optimize_ticks(Dtime_all[1],Dtime_all[end])[1];
+x = [0,0,0,1,1,1,2,2,2,3,3,3,4,4,4,5,5,5,6,6,6,7,7,7,0,0,1,1,2,2,3,3,4,4,5,5,6,6,7,7,0,0,0,1,1,1,2,2,2,3,3,3,4,4,4,5,5,5,6,6,6,7,7,7] .+ 1;
+y = [0,1,2,2,1,0,0,1,2,2,1,0,0,1,2,2,1,0,0,1,2,2,1,0,3,4,4,3,4,3,3,4,4,3,3,4,4,3,4,3,5,6,7,7,6,5,5,6,7,7,6,5,5,6,7,7,6,5,5,6,7,7,6,5] .+ 1;
+
+# Until I figure how to deal with missing data 
+SWC_daily = replace(SWC_daily, missing=>0.0); SWC_daily_mean = replace(SWC_daily_mean, missing=>0.0); SWC_daily_std = replace(SWC_daily_std, missing=>0.0);
+Tsoil_daily = replace(Tsoil_daily, missing=>0.0); Tsoil_daily_mean = replace(Tsoil_daily_mean, missing=>0.0); Tsoil_daily_std = replace(Tsoil_daily_std, missing=>0.0);
+
+# Create Scene and 2D axis
+scene, layout = layoutscene(10, 24, 10, resolution = (1500, 900))
+ax = Array{LAxis}(undef,4)
+sl = layout[10, 1:23] = LSlider(scene, range=1:n_all)
+
+Text_date = layout[1,1:23] = LText(scene, text= lift(X->Dates.format(Dtime_all[X], "e, dd u yyyy"), sl.value), textsize=40)
+# SWC + Precip time-series (bottom)
+ax[3] = layout[8:9, 1:23] = LAxis(scene, ylabel = "Precip", yaxisposition = :right, xticklabelsvisible = false, xticksvisible = false, ylabelpadding = -10, xgridvisible = false, ygridvisible = false, yticklabelalign = (:left, :center))
+barplot!(ax[3], Dtime_all_rata[1:end], Precip_daily[1:end], color = :blue)
+scatter!(ax[3], lift(X-> [Point2f0(Dtime_all_rata[X], 0)], sl.value), marker = :vline, markersize = Vec2f0(0.5,5), color = :black)
+xlims!(ax[3], (Dtime_all_rata[1], Dtime_all_rata[end])); ylims!(ax[3], (0, 2))
+
+ax[1] = layout[8:9, 1:23] = LAxis(scene, ylabel="SWC", xlabel="Date", ylabelpadding = -25)
+lines!(ax[1], Dtime_all_rata[1:end], SWC_daily_mean[1:end], color = :blue, linewidth = 2)
+band!(ax[1], Dtime_all_rata[1:end], SWC_daily_mean[1:end] + SWC_daily_std[1:end], SWC_daily_mean[1:end] - SWC_daily_std[1:end], color = color = RGBAf0(0,0,1,0.3))
+xlims!(ax[1], (Dtime_all_rata[1], Dtime_all_rata[end]));
+ax[1].xticks[] = ManualTicks(datetime2rata.(dateticks) , Dates.format.(dateticks, "yyyy-mm-dd"))
+
+
+# Tsoil + Rsoil time-series (bottom)
+ax[2] = layout[6:7, 1:23] = LAxis(scene, ylabel="Ts", ylabelpadding = -25, xticklabelsvisible = false, xticksvisible = false)
+lines!(ax[2], Dtime_all_rata[1:end], Tsoil_daily_mean[1:end], color = :red, linewidth = 2)
+band!(ax[2], Dtime_all_rata[1:end], Tsoil_daily_mean[1:end] + Tsoil_daily_std[1:end], Tsoil_daily_mean[1:end] - Tsoil_daily_std[1:end], color = RGBAf0(1,0,0,0.3))
+scatter!(ax[2], lift(X-> [Point2f0(Dtime_all_rata[X], 0)], sl.value), marker = :vline, markersize = Vec2f0(0.5,5), color = :black)
+xlims!(ax[2], (Dtime_all_rata[1], Dtime_all_rata[end]));
+ax[2].xticks[] = ManualTicks(datetime2rata.(dateticks) , Dates.format.(dateticks, "yyyy-mm-dd"))
+
+ax[4] = layout[6:7, 1:23] = LAxis(scene, ylabel = "Rs", xticklabelsvisible = false, xticksvisible = false, xgridvisible = false, ygridvisible = false, yaxisposition = :right, ylabelpadding = -10, yticklabelalign = (:left, :center))
+lines!(ax[4], Dtime_all_rata[1:end], Rsoil_daily_mean[1:end], color = :black)
+xlims!(ax[4], (Dtime_all_rata[1], Dtime_all_rata[end]));
+
+ax3D = Array{LRect}(undef,3)
+cbar = Array{LColorbar}(undef,3)
+
+ax3D[1] = layout[1:7, 1:8] = LRect(scene, visible = false);
+scene3D_1 = Scene(scene, lift(IRect2D, ax3D[1].layoutnodes.computedbbox), camera = cam3d!, raw = false, show_axis = true);
+surface!(scene3D_1, 0:7, 0:7, elev, color = lift(X-> GLMakie.vec2color(Matrix(sparse(x, y, SWC_daily[X,:])), Reverse(:lighttest), (0,1)), sl.value), shading = false, limits = Rect(0, 0, 0, 7, 7, 1.5));
+x_or = [0,0,0]; Ylen = 7; Zlen = 1; Xlen = 7;
+mesh!(scene3D_1, lift(X-> HyperRectangle(Vec3f0(x_or), Vec3f0(Xlen, Ylen, Wtable_daily[X])), sl.value) , color = RGBAf0(0,0,1,0.2));
+cbar[1] = layout[2, 2:7] = LColorbar(scene, height = 20, limits = (0.35, 0.48), label = "SWC", colormap = :lighttest, vertical = false, labelpadding = -5);
+
+ax3D[2] = layout[1:7, 9:16] = LRect(scene, visible = false);
+scene3D_2 = Scene(scene, lift(IRect2D, ax3D[2].layoutnodes.computedbbox), camera = cam3d!, raw = false, show_axis = true);
+surface!(scene3D_2, 0:7, 0:7, elev, color = lift(X-> GLMakie.vec2color(Matrix(sparse(x, y, Tsoil_daily[X,:])), Reverse(:lighttest), (0,1)), sl.value), shading = false, limits = Rect(0, 0, 0, 7, 7, 1.5));
+x_or = [0,0,0]; Ylen = 7; Zlen = 1; Xlen = 7;
+mesh!(scene3D_2, lift(X-> HyperRectangle(Vec3f0(x_or), Vec3f0(Xlen, Ylen, Wtable_daily[X])), sl.value) , color = RGBAf0(0,0,1,0.2));	
+cbar[2] = layout[2, 10:15] = LColorbar(scene, height = 20, limits = (0, 10), label = "Tsoil", colormap = :lighttest, vertical = false, labelpadding = -5);
+
+ax3D[3] = layout[1:7, 17:24] = LRect(scene, visible = false);
+scene3D_3 = Scene(scene, lift(IRect2D, ax3D[3].layoutnodes.computedbbox), camera = cam3d!, raw = false, show_axis = true);
+surface!(scene3D_3, 0:7, 0:7, elev, color = lift(X-> GLMakie.vec2color(Matrix(sparse(x, y, Rsoil_daily[X,:])), Reverse(:lighttest), (0,1)), sl.value), shading = false, limits = Rect(0, 0, 0, 7, 7, 1.5));
+x_or = [0,0,0]; Ylen = 7; Zlen = 1; Xlen = 7;
+mesh!(scene3D_3, lift(X-> HyperRectangle(Vec3f0(x_or), Vec3f0(Xlen, Ylen, Wtable_daily[X])), sl.value) , color = RGBAf0(0,0,1,0.2));	  
+cbar[3] = layout[2, 18:23] = LColorbar(scene, height = 20, limits = (0, 1), label = "Rsoil", colormap = :lighttest, vertical = false, labelpadding = -5);
+
+
+axis1 = scene3D_1[Axis]
+axis1.names.axisnames = ("Coordinate x (m)","Coordinate y (m)","z")
+axis1[:names][:textsize] = (20.0,20.0,20.0) # same as axis.names.textsize
+axis1[:ticks][:textsize] = (20.0,20.0,20.0)
+axis1[:ticks][:ranges_labels] = (([1.0,3.0,5.0,7.0], [1.0,3.0,5.0,7.0], [1.0, 1.25, 1.5, 1.75, 2.0]), (["75","50","25","0"], ["75","50","25","0"], ["1.00", "1.25", "1.50", "1.75", "2.00"]))
+
+axis2 = scene3D_2[Axis]
+axis2.names.axisnames = ("Coordinate x (m)","Coordinate y (m)","z")
+axis2[:names][:textsize] = (20.0,20.0,20.0) # same as axis.names.textsize
+axis2[:ticks][:textsize] = (20.0,20.0,20.0)
+axis2[:ticks][:ranges_labels] = (([1.0,3.0,5.0,7.0], [1.0,3.0,5.0,7.0], [1.0, 1.25, 1.5, 1.75, 2.0]), (["75","50","25","0"], ["75","50","25","0"], ["1.00", "1.25", "1.50", "1.75", "2.00"]))
+
+axis3 = scene3D_3[Axis]
+axis3.names.axisnames = ("Coordinate x (m)","Coordinate y (m)","z")
+axis3[:names][:textsize] = (20.0,20.0,20.0) # same as axis.names.textsize
+axis3[:ticks][:textsize] = (20.0,20.0,20.0)
+axis3[:ticks][:ranges_labels] = (([1.0,3.0,5.0,7.0], [1.0,3.0,5.0,7.0], [1.0, 1.25, 1.5, 1.75, 2.0]), (["75","50","25","0"], ["75","50","25","0"], ["1.00", "1.25", "1.50", "1.75", "2.00"]))
+
+
+scene
+
+
+
